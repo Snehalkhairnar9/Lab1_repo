db421fdf4daf0d6cdc16b123d1bab091f4852487
Snehalkhairnar9 <L00163434@student.lyit.ie>
2021-10-13 15:16:51 +0100

    lab

---
+++ new/lab.py
@@ -0,0 +1,15 @@
+# File          : pythonproject.py
+# Author        : Snehal Khairnar
+# Version       : v1.0
+# Created Date  : 08/10/2021
+# Description   : Testing the functionality of List, Tuples
+# Licensing     : Snehal Khairnar, LYIT
+# ----------------------------------
+
+a = 50
+b = 60
+print(a + b)
+print(b - a)
+print(a * b)
+
+
